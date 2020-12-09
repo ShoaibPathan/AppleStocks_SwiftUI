c7ad4742cecf4ab01282f1b9faca9967bc477ea6
Ashish Tyagi <ashisht@dewsolutions.in>
2020-12-09 13:34:27 +0530

    Initial Commit

---
+++ AppleStocks_SwiftUI/AppleStocks_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleStocks_SwiftUIApp.swift
+//  AppleStocks_SwiftUI
+//
+//  Created by Ashish Tyagi on 09/12/20.
+//
+
+import SwiftUI
+
+@main
+struct AppleStocks_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
